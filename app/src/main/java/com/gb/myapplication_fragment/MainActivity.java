df7ffe98e9faca5bd87de0fd93e6307ad4376f2c
Калпакбаев <kozha_1992@mail.ru>
2022-02-24 21:51:41 +0500

    Добавлен 2 кнопки меню и обработан,

--- app/src/main/java/com/gb/myapplication_fragment/MainActivity.java
+++ app/src/main/java/com/gb/myapplication_fragment/MainActivity.java
@@ -1,10 +1,13 @@
 package com.gb.myapplication_fragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
             NotesFragment notesFragment = NotesFragment.newInstance();
            getSupportFragmentManager().beginTransaction().replace(R.id.note,notesFragment).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case (R.id.action_about):{
+                getSupportFragmentManager().beginTransaction().replace(R.id.note,new AboutFragment()).commit();
+                return true;
+            }
+            case (R.id.action_exit):{
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
