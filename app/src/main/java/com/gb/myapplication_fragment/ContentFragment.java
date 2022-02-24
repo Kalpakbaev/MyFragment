0b8434f548e80f65f552bc474dbc5eeca7157379
Калпакбаев <kozha_1992@mail.ru>
2022-02-25 01:17:10 +0500

    Разработан options menu в активити и на фрагменте

--- app/src/main/java/com/gb/myapplication_fragment/ContentFragment.java
+++ app/src/main/java/com/gb/myapplication_fragment/ContentFragment.java
@@ -6,8 +6,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class ContentFragment extends Fragment {
@@ -25,8 +29,24 @@ public class ContentFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_content_fragment,menu);
+        menu.findItem(R.id.action_about).setVisible(false);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_toast:{
+                Toast.makeText(requireContext(), "Toast", Toast.LENGTH_LONG).show();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contents, container, false);
     }
@@ -36,6 +56,7 @@ public class ContentFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         note = getArguments().getParcelable(ARG_KEY);
+        setHasOptionsMenu(true);
 
         getChildFragmentManager().beginTransaction().replace(R.id.container_child,ContentChildFragment.newInstance(note)).addToBackStack("").commit();
 
